0732d4fa89b6625692b8e83064d9698d127cab6d
ARIANY <ariany_mendes@yahoo.com>
2020-03-04 12:22:29 -0500

    add routes for products and single product

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -1,2 +1,18 @@
 class Api::CapstonesController < ApplicationController
+
+def pp
+  @products = Product.first
+  render 'pink.json.jb'
+end
+
+def pb
+  @products = Product.second
+  render 'black.json.jb'
+end
+
+def eg
+  @products = Product.last
+  render 'green.json.jb'
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  
+   namespace :api do
+    get "/pen" => "capstones#pp"
+
+    get "/pencil" => "capstones#pb"
+
+    get "/eraser" => "capstones#eg"
+
+   end
 end
