5597ef62087e0d12840e6870f3e8ba729cd39264
ARIANY <ariany_mendes@yahoo.com>
2020-03-04 14:05:55 -0500

    add new items  through seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+
+products = Product.new({name: "calculater", price: 25, image_url: "none", description: "zero"})
+products.save
+
+products = Product.new({name: "iphone_6", price: 250, image_url: "none", description: "zero"})
+products.save
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
