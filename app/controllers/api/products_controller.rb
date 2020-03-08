111069ce593f2ac3cbb8c7091b2c944e4177cd12
ARIANY <ariany_mendes@yahoo.com>
2020-03-08 12:01:38 -0400

    add delete

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -17,16 +17,23 @@ class Api::ProductsController < ApplicationController
         image: params[:input_image]
     )
       @products.save
-      render 'create.json.jb'
+      render 'show.json.jb'
     end
 
     def update
       id = params[:id]
       @products = Product.find_by(id: id),
-      @products.name =  params[:input_name],
-      @products.price = params[:input_price],
-      @products.image = params[:input_image]
-
+      @products.update(
+      @products.name =  params[:input_name] || @products.name,
+      @products.price = params[:input_price] || @products.price,
+      @products.image = params[:input_image] || @products.image
+      )
       render 'update.json.jb'
     end
+
+    def destroy
+      @products = Product.find_by(id: params[:id],
+      @products.destroy
+      render json: {message: "Product has been removed"}
+
 end
