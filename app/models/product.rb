ec8bae949085cbc2bff348769165e8f099fc2462
ARIANY <ariany_mendes@yahoo.com>
2020-03-09 21:49:01 -0400

    add products.json.jb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    price < 10
+  end
+
+  def tax
+    tax_rate = 0.09
+    price * tax_rate
+  end
+
+  def full_price
+    price + tax
+  end
 end
